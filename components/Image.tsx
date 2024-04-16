87e73ed22589ac8a9374404a3fc7e5a8f96c6cc0
Lanxiang Hu <hlxde1@yahoo.com>
2024-04-16 06:18:38 -0700

    add static content build

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,5 +1,5 @@
 import NextImage, { ImageProps } from 'next/image'
 
-const Image = ({ src, ...rest }: ImageProps) => <NextImage src={`/MLSys-UCSD${src}`} {...rest} />
+const Image = ({ ...rest }: ImageProps) => <NextImage {...rest} />
 
 export default Image
